a749e7473e0e858cae837a8b37f34208260acf45
jepthoniq <106372829+jepthoniq@users.noreply.github.com>
2022-05-27 03:54:37 -0700

    Update مالك.py

--- jepthon/plugins/مالك.py
+++ jepthon/plugins/مالك.py
@@ -2,8 +2,8 @@ from datetime import datetime
 
 from telethon.utils import get_display_name
 
-from userbot import jmthon
-from userbot.core.logger import logging
+from jepthon import jmthon
+from jepthon.core.logger import logging
 
 from ..Config import Config
 from ..core import CMD_INFO, PLG_INFO
