f8df5da681cf2237976111c4205059a445f20744
jepthoniq <106372829+jepthoniq@users.noreply.github.com>
2022-05-27 03:09:03 -0700

    Update nsfw.py

--- jepthon/assistant/nsfw.py
+++ jepthon/assistant/nsfw.py
@@ -4,7 +4,7 @@ from telethon import Button
 from telethon.errors import MessageNotModifiedError
 from telethon.events import CallbackQuery
 
-from userbot import jmthon
+from jepthon import jmthon
 
 from ..Config import Config
 from ..core.logger import logging
