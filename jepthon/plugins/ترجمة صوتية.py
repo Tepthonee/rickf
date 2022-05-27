25ecb1dcd28d767ed7f9d708945cbbc1fdc46605
jepthoniq <106372829+jepthoniq@users.noreply.github.com>
2022-05-27 03:06:02 -0700

    Update ترجمة صوتية.py

--- jepthon/plugins/ترجمة صوتية.py
+++ jepthon/plugins/ترجمة صوتية.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 from gtts import gTTS
 
-from userbot import jmthon
+from jepthon import jmthon
 
 from ..core.managers import edit_delete, edit_or_reply
 from . import deEmojify, reply_id
