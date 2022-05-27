170d4282842a8dc10fff1886ff06f7762da7a4c0
jepthoniq <106372829+jepthoniq@users.noreply.github.com>
2022-05-27 02:18:37 -0700

    Update __main__.py

--- jepthon/__main__.py
+++ jepthon/__main__.py
@@ -1,7 +1,7 @@
 import sys
 
-import userbot
-from userbot import BOTLOG_CHATID, HEROKU_APP, PM_LOGGER_GROUP_ID
+import jepthon
+from jepthon import BOTLOG_CHATID, HEROKU_APP, PM_LOGGER_GROUP_ID
 
 from .Config import Config
 from .core.logger import logging
