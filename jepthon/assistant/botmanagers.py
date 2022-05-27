e129905e6a122d7103e761ee3ff5858215bf42c7
jepthoniq <106372829+jepthoniq@users.noreply.github.com>
2022-05-27 03:11:21 -0700

    Update botmanagers.py

--- jepthon/assistant/botmanagers.py
+++ jepthon/assistant/botmanagers.py
@@ -3,7 +3,7 @@ from math import floor
 
 from telethon.utils import get_display_name
 
-from userbot import jmthon
+from jepthon import jmthon
 
 from ..Config import Config
 from ..core.logger import logging
