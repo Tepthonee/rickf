6ea5406abb118e6444972d86207209ab96b7fe6f
jepthoniq <106372829+jepthoniq@users.noreply.github.com>
2022-05-27 05:10:40 -0700

    Update bot_pms.py

--- jepthon/assistant/bot_pms.py
+++ jepthon/assistant/bot_pms.py
@@ -8,7 +8,7 @@ from telethon.errors import UserIsBlockedError
 from telethon.events import CallbackQuery, StopPropagation
 from telethon.utils import get_display_name
 
-from userbot import Config, jmthon
+from jepthon import Config, jmthon
 
 from ..core import check_owner, pool
 from ..core.logger import logging
