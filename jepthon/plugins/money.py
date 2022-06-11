d87c0bea249a688213f96ed411019f9956435af6
redaiq90 <60717660+redaiq90@users.noreply.github.com>
2022-06-11 10:28:59 +0300

    Update money.py

--- jepthon/plugins/money.py
+++ jepthon/plugins/money.py
@@ -864,7 +864,7 @@ async def SpaceBank(message):
                 x.write(f"name:{message.chat.first_name}:account:{s}:bank:SpaceBank.")
 
 
-                await edit_or_reply(message,text=f"<strong>Done Create Banking Account! Account Detials :\nAccount Id : {s}\nBalance : 50 💵.\nBank Name : SpaceBank.</strong>",parse_mode="html")
+                don = await edit_or_reply(message,text=f"<strong>Done Create Banking Account! Account Detials :\nAccount Id : {s}\nBalance : 50 💵.\nBank Name : SpaceBank.</strong>",parse_mode="html")
 
 
             with open(f"c{message.chat.id}.txt","a")as xs:
