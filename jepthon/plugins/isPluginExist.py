c452813e47f04facea13f3ce07df26e4ae44b547
redaiq90 <60717660+redaiq90@users.noreply.github.com>
2022-06-13 20:37:29 +0300

    Create isPluginExist.py

---
+++ jepthon/plugins/isPluginExist.py
@@ -0,0 +1,12 @@
+import os.path
+from jepthon import jmthon
+from ..core.managers import edit_delete, edit_or_reply
+
+
+@jmthon.ar_cmd(pattern="موجود؟(?:\s|$)([\s\S]*)")
+
+async def _(event)
+    input_str = event.pattern_match.group(1)
+    if input_str is None:
+        await edit_delete(event, "قم بكتابة اسم البلوكن")
+    return edit_or_reply(event, f"{input_str}\n{os.path.exists(jepthon/plugins/{input_str}.py))
