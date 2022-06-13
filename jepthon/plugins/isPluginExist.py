26aab96fdd7f0d2c70c82b384b6971484f2da33d
redaiq90 <60717660+redaiq90@users.noreply.github.com>
2022-06-13 20:55:04 +0300

    Update isPluginExist.py

--- jepthon/plugins/isPluginExist.py
+++ jepthon/plugins/isPluginExist.py
@@ -12,4 +12,4 @@ async def _(event):
     input_str = event.pattern_match.group(1)
     if input_str is None:
         await edit_delete(event, "قم بكتابة اسم البلوكن")
-    return edit_or_reply(event, f"isEx({input_str})")
+    return await edit_or_reply(event, f"isEx({input_str})")
