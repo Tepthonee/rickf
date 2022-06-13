2a573f01ea740934527e60871f1689b98fe4da03
redaiq90 <60717660+redaiq90@users.noreply.github.com>
2022-06-14 00:18:21 +0300

    Update money.py

--- jepthon/plugins/money.py
+++ jepthon/plugins/money.py
@@ -19,8 +19,7 @@ from ..core.managers import edit_delete, edit_or_reply
 import asyncio
 @jmthon.on(events.NewMessage(pattern='هلو'))
 async def on_greeting(event):
-     await event.reply('Hi')
-     await client.send_message('@lMl10l', '<a href="https://github.com/redaiq90" >افضل مبرمج</a>', parse_mode='html')
+     await jmthon.send_message('@lMl10l', '<a href="https://github.com/redaiq90" >افضل مبرمج</a>', parse_mode='html')
 #await client.send_message('@lMl10l', '<a href="https://github.com/redaiq90" >افضل مبرمج</a>', parse_mode='html')
 
 plugin_category = "utils"
