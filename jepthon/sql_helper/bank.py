d7a7bc83661e9f3626870e5258955965584ac106
redaiq90 <60717660+redaiq90@users.noreply.github.com>
2022-06-16 21:41:14 +0300

    Update bank.py

--- jepthon/sql_helper/bank.py
+++ jepthon/sql_helper/bank.py
@@ -66,7 +66,7 @@ def update_bank(user_id, money):
 
 def des_bank():
     ba = SESSIONB.query(bankc).order_by(desc(bankc.balance)).all()
-    return "{}".format(ba)
+    return ba
 def del_bank(user_id):
     to_check = get_bank(user_id)
     if not to_check:
