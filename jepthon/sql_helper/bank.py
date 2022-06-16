b5769ee359477fc5599b3b991b422ae7e3e36fc8
redaiq90 <60717660+redaiq90@users.noreply.github.com>
2022-06-16 23:26:49 +0300

    Update bank.py

--- jepthon/sql_helper/bank.py
+++ jepthon/sql_helper/bank.py
@@ -68,7 +68,7 @@ def del_bank(user_id):
     to_check = get_bank(user_id)
     if not to_check:
         return False
-    SESSIONB.query(bankc).filter(bankc.user_id == user_id).delete(synchronize_session=False)
+    SESSIONB.query(bankc).filter(bankc.user_id == str(user_id)).delete(synchronize_session=False)
     SESSIONB.commit()
     return True
 
