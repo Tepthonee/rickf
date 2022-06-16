06e5afe557d0f77940f274b3f32b4e625bea3206
redaiq90 <60717660+redaiq90@users.noreply.github.com>
2022-06-16 13:57:25 +0300

    Update money.py

--- jepthon/plugins/money.py
+++ jepthon/plugins/money.py
@@ -294,7 +294,7 @@ async def thief(message):
     jep = int(be) - int(rt)
     update_bank(user.id, jep)
     jepthon = mee.first_name.replace("\u2060", "") if mee.first_name else mee.username
-    await jmthon.tgbot.send_message(str(user.id), f"لقد سرقك [{jepthon}](tg://user?id={mee.id})\n {rt} 💵")
+    await jmthon.tgbot.send_message(int(user.id), f"لقد سرقك [{jepthon}](tg://user?id={mee.id})\n {rt} 💵")
     await edit_or_reply(message, f"لقد سرقت {rt} من {user.first_name}")
     ga = int(rt) + int(ppe)
     update_bank(mee.id, ga)
