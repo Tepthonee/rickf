7abce5e2033a0f6aa15f41edcdd17225dfc89e76
redaiq90 <60717660+redaiq90@users.noreply.github.com>
2022-06-16 20:53:45 +0300

    Update bank.py

--- jepthon/sql_helper/bank.py
+++ jepthon/sql_helper/bank.py
@@ -71,7 +71,7 @@ def des_bank():
     #order_by = re.sub('[^0-9a-zA-Z]+', '', order_by) + desc
     #return SESSIONB.query(bankc).get(desc(bankc.balance))
     query_obj = SESSIONB.query(bankc)
-    desc_expression = sqlalchemy.sql.expression.desc(bankc.balance)
+    desc_expression = desc(bankc.balance)
     order_by_query = query_obj.order_by(desc_expression)
     return order_by_query
 def del_bank(user_id):
