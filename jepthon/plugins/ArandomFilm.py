b17e538f2715a6e20f0c1ce1c37025f2d0970d2f
redaiq90 <60717660+redaiq90@users.noreply.github.com>
2022-06-19 14:15:08 +0300

    Update ArandomFilm.py

--- jepthon/plugins/ArandomFilm.py
+++ jepthon/plugins/ArandomFilm.py
@@ -10,14 +10,21 @@ ia = Cinemagoer()
 @jmthon.ar_cmd(pattern="فلم")
 async def rfilm(event):
     for _ in range(100):
-        movieID = f'{randint(1,9999999):07}'
+        movieID = f"{randint(1,9999999):07}"
         url = f'https://www.imdb.com/title/tt{movieID}'
         r = requests.get(url)
         if r.status_code != 200:
             continue
 
         movie = ia.get_movie(movieID)
-        votes = movie.get('votes', None)
-        rating = movie.get('rating', None)
-        await edit_or_reply(event, f"{movie}\n{movie.title}{votes}\n{rating}")
+        year = movie.get('year')
+        rating = movie.get('rating', "لا يوجد")
+        movien = movie.get('title')
+        moviep = movie.get('cover url')
+        moviet = f"الاسم: {movien}\nالسنة: {year}\nالتقييم: {rating}"
+        await jmthon.send_file(
+                message.chat_id,
+                str(moviep),
+                caption=moviet,
+                )
         break
