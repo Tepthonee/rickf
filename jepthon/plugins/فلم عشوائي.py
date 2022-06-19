bc636075c3544d223fdba86d1edeaab3e166f640
redaiq90 <60717660+redaiq90@users.noreply.github.com>
2022-06-19 13:02:17 +0300

    Update فلم عشوائي.py

--- jepthon/plugins/فلم عشوائي.py
+++ jepthon/plugins/فلم عشوائي.py
@@ -6,16 +6,14 @@ import asyncio
 from ..core.managers import edit_delete, edit_or_reply
 
 ia = Cinemagoer()
+
 @jmthon.ar_cmd(pattern="فلم")
 async def rfilm(event):
-    for _ in range(100):
-        movieID = f'{randint(1,9999999):07}'
-        url = f'https://www.imdb.com/title/tt{movieID}'
-        r = requests.get(url)
-        if r.status_code != 200:
-            continue
-
+    movieID = f'{randint(1,9999999):07}'
+    url = f'https://www.imdb.com/title/tt{movieID}'
+    r = requests.get(url)
+    if r.status_code != 200:
         movie = ia.get_movie(movieID)
         votes = movie.get('votes', None)
         rating = movie.get('rating', None)
-     await edit_or_reply(event, str(movie\nvotes\nrating))        
+        await edit_or_reply(event, f"{movie}\n{votes}\n{rating}")
