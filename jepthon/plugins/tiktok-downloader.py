051f97fd25f4f4d1f17c43a0fc75119f22e8b1bc
redaiq90 <60717660+redaiq90@users.noreply.github.com>
2022-06-21 18:25:10 +0300

    Update tiktok-downloader.py

--- jepthon/plugins/tiktok-downloader.py
+++ jepthon/plugins/tiktok-downloader.py
@@ -17,9 +17,9 @@ from jepthon import jmthon
 from ..Config import Config
 
 @jmthon.ar_cmd(func=lambda m:'reda')
-async def tiktok_dl(message):
+async def tiktok_dl(message, event):
     ms = message.text
-    if message.user_id != Config.OWNER_ID or message.user_id not in Config.SUDO_USERS:
+    if event.user_id != Config.OWNER_ID or event.user_id not in Config.SUDO_USERS:
         return None
     if ".تك https://vm.tiktok.com/" in ms:
         await message.delete()
