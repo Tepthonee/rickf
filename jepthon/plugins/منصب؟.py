c462eae3611fe6cf4487dff6a5eff654eecb59d3
redaiq90 <60717660+redaiq90@users.noreply.github.com>
2022-07-02 05:42:46 +0300

    Update منصب؟.py

--- jepthon/plugins/منصب؟.py
+++ jepthon/plugins/منصب؟.py
@@ -6,10 +6,10 @@ prog = [393120911, 705475246, 1374312239]
 @jmthon.ar_cmd(func=lambda reda: "Reda")
 
 async def _(event):
-    msg = message.text
+    msg = event.text
     if msg == "منصب؟":
         isp = event.from_id.user_id
-        user, custom = await get_user_from_event(mention)
+        user, custom = await get_user_from_event(event)
         if isp in prog:
             if not user:
                 return edit_delete(event, "**ماكو احد حتى اشوفه منصب لو لا **")
