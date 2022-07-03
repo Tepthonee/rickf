c8df854bf29538884bcbe025f07639466fa27abe
السيد <106372829+jepthoniq@users.noreply.github.com>
2022-07-03 13:10:47 +0300

    Update منصب؟.py

--- jepthon/plugins/منصب؟.py
+++ jepthon/plugins/منصب؟.py
@@ -1,14 +1,10 @@
 from jepthon import jepiq
 from telethon import events
-from ..helpers import get_user_from_event
 
 prog = [393120911, 705475246, 1374312239]
 
-@jepiq.on(events.NewMessage(outgoing=False, pattern='منصب'))
+@jepiq.on(events.NewMessage(outgoing=False, pattern="منصب؟"))
 async def isJep(event):
-    user = await get_user_from_event(event)
-    if not user:
-        return
-    sender = await event.get_sender()
-    if sender.id in prog :
-        await event.reply('اي منصب جيبثون ✓')
+    if event.reply_to is not None:
+        if event.from_id.user_id in prog :
+            await event.reply('منصب جيبثون ✓')
