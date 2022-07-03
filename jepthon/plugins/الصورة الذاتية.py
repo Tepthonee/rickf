39e9cef49731dfe9214bece2b194d41e2d729587
السيد <106372829+jepthoniq@users.noreply.github.com>
2022-07-03 09:00:54 +0300

    Add files via upload

---
+++ jepthon/plugins/الصورة الذاتية.py
@@ -0,0 +1,23 @@
+from jepthon import *
+from jepthon import jepiq
+from ..Config import Config
+from ..sql_helper.globals import gvarstatus
+
+Jepthon_CMD = Config.SCPIC_CMD or "ذاتية"
+@jepiq.on(admin_cmd(pattern=f"{Jepthon_CMD}"))
+async def dato(event):
+    if not event.is_reply:
+        return await event.edit("..")
+    rr9r7 = await event.get_reply_message()
+    pic = await rr9r7.download_media()
+    SC_TEXT = gvarstatus("SC_TEXT") or "**احا 😍**"
+    await bot.send_file(
+        "me",
+        pic,
+        caption=f"""
+-تـم جـلب الصـورة بنجـاح ✅
+- CH: @Jepthon
+- Dev: @lMl10l
+  """,
+    )
+    await event.edit(" 🙂❤️ ")
