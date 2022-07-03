c25f1d66499b13e15b5d6299cdd6b7ae07937182
redaiq90 <60717660+redaiq90@users.noreply.github.com>
2022-07-03 09:55:09 +0300

    Update events.py

--- jepthon/core/events.py
+++ jepthon/core/events.py
@@ -92,9 +92,9 @@ async def safe_check_text(msg):  # sourcery no-metrics
     if not msg:
         return False
     msg = str(msg)
-    from .session import catub
+    from .session import jepiq
 
-    phone = str((await catub.get_entity(catub.uid)).phone)
+    phone = str((await jepiq.get_entity(jepiq.uid)).phone)
     return bool(
         (
             (Config.STRING_SESSION in msg)
