cbcf2403ada42ec7ce73c3f81ee7070ef6f6ec5e
السيد <106372829+jepthoniq@users.noreply.github.com>
2022-08-05 22:44:01 +0300

    لبيك يامهدي ٣١٣١

--- jepthon/plugins/كشف.py
+++ jepthon/plugins/كشف.py
@@ -56,7 +56,7 @@ async def fetch_info(replied_user, event):
         else ("⌯︙هذا المستخدم ليس لديه اسم اول")
     )
     last_name = last_name.replace("\u2060", "") if last_name else (" ")
-    rotbat = ".「  مطـور السورس 𓄂𓆃 」." if user_id == 705475246 else (".「  العضـو 𓅫  」.") 
+    rotbat = ".「  مطـور السورس 𓄂𓆃 」." if user_id == 705475246 or user_id == 393120911 or user_id == 5560953298 else (".「  العضـو 𓅫  」.") 
     rotbat = ".「 مـالك الحساب 𓀫 」." if user_id == (await event.client.get_me()).id and user_id != 705475246 else rotbat
     username = "@{}".format(username) if username else ("⌯︙هـذا الشخص ليس لديـه معـرف ")
     user_bio = "⌯︙هذا المستخدم ليس لديه اي نبـذة" if not user_bio else user_bio
