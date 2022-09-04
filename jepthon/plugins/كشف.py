1ebb2c5142b2157d9377e1e21a1370e9a5f27e41
السيد <106372829+jepthoniq@users.noreply.github.com>
2022-09-04 17:20:47 +0300

    Update كشف.py

--- jepthon/plugins/كشف.py
+++ jepthon/plugins/كشف.py
@@ -146,38 +146,35 @@ async def _(event):
     await edit_or_reply(catevent, caption)
 
 
-@jepiq.on(admin_cmd(pattern=f"{ID_EDIT}(?:\s|$)([\s\S]*)"))
+@jepiq.ar_cmd(pattern="ايدي(?: |$)(.*)",
+    command=("ايدي", plugin_category),
+    info={
+        "header": "لـ عـرض معلومـات الشخـص",
+        "الاستـخـدام": " {tr}ايدي بالـرد او {tr}ايدي + معـرف/ايـدي الشخص",
+    },
+)
 async def who(event):
     "Gets info of an user"
+    cat = await edit_or_reply(event, "⇆")
     if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
         os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
-    replied_user, reason = await get_user_from_event(event)
-    if not replied_user:
-        return
-    cat = await edit_or_reply(event, "**⌯︙يتم استخراج معلومات المستخدم **")
-    replied_user = await event.client(GetFullUserRequest(replied_user.id))
+    replied_user = await get_user_from_event(event)
     try:
         photo, caption = await fetch_info(replied_user, event)
     except AttributeError:
-        return await edit_or_reply(cat, "**⌯︙لم يتم العثور على معلومات لهذا المستخدم **")
-    message_id_to_reply = await reply_id(event)
+        return await edit_or_reply(cat, "**- لـم استطـع العثــور ع الشخــص**")
+    message_id_to_reply = event.message.reply_to_msg_id
+    if not message_id_to_reply:
+        message_id_to_reply = None
     try:
-        await event.client.send_file(
-            event.chat_id,
-            photo,
-            caption=caption,
-            link_preview=False,
-            force_document=False,
-            reply_to=message_id_to_reply,
-            parse_mode="html",
-        )
+        await event.client.send_file(            event.chat_id,            photo,            caption=caption,            link_preview=False,            force_document=False,            reply_to=message_id_to_reply,            parse_mode="html",        )
         if not photo.startswith("http"):
             os.remove(photo)
         await cat.delete()
     except TypeError:
         await cat.edit(caption, parse_mode="html")
 #كـتابة  @lMl10l
-#تعديل وترتيب  @SBB_B
+#تعديل وترتيب  @lMl10l
 @jepiq.ar_cmd(
     pattern="رابط الحساب(?:\s|$)([\s\S]*)",
     command=("رابط الحساب", plugin_category),
