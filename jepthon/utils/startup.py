ae696ffe6e73f36d81a65388bebb9d98400f462d
السيد <106372829+jepthoniq@users.noreply.github.com>
2022-09-09 09:27:45 -0700

    Update startup.py

--- jepthon/utils/startup.py
+++ jepthon/utils/startup.py
@@ -99,10 +99,10 @@ async def startupmessage():
 
 
 async def mybot():
-    JEPTH_USER = bot.me.first_name
-    The_razan = bot.uid
-    rz_ment = f"[{JEPTH_USER}](tg://user?id={The_razan})"
-    f"ـ {rz_ment}"
+    JEPTH_USER = jepiq.me.first_name
+    The_noon = jepiq.uid
+    jep_ment = f"[{JEPTH_USER}](tg://user?id={The_noon})"
+    f"ـ {jep_ment}"
     f"⪼ هذا هو بوت خاص بـ {rz_ment} يمكنك التواصل معه هنا"
     starkbot = await jepiq.tgbot.get_me()
     perf = "[ جيبثون ]"
@@ -112,11 +112,11 @@ async def mybot():
         print("تم تشغيل البوت")
     else:
         try:
-            await bot.send_message("@BotFather", "/setinline")
+            await jepiq.send_message("@BotFather", "/setinline")
             await asyncio.sleep(1)
-            await bot.send_message("@BotFather", botname)
+            await jepiq.send_message("@BotFather", botname)
             await asyncio.sleep(1)
-            await bot.send_message("@BotFather", perf)
+            await jepiq.send_message("@BotFather", perf)
             await asyncio.sleep(2)
         except Exception as e:
             print(e)
