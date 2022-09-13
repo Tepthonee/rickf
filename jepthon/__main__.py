563e0629fbb73140c8a44e13a9f4abe54d0f6ba1
السيد <106372829+jepthoniq@users.noreply.github.com>
2022-09-13 10:31:55 +0300

    Update __main__.py

--- jepthon/__main__.py
+++ jepthon/__main__.py
@@ -30,7 +30,14 @@ try:
 except Exception as e:
     LOGS.error(f"{str(e)}")
     sys.exit()
-    
+
+try:
+    LOGS.info("يتم تفعيل وضع الانلاين")
+    jepiq.loop.run_until_complete(mybot())
+    LOGS.info("تم تفعيل وضع الانلاين بنجاح ✓")
+except Exception as jep:
+    LOGS.error(f"- {jep}")
+    sys.exit()    
 
 
 class CatCheck:
