bb63f627d09bb5ed9c984a391436a5fcc68a4de3
روجر <104784913+rogerpq@users.noreply.github.com>
2022-09-27 19:41:28 +0300

    Update لستة.py

--- jepthon/plugins/لستة.py
+++ jepthon/plugins/لستة.py
@@ -21,7 +21,7 @@ BTN_URL_REGEX = re.compile(r"(\[([^\[]+?)\]\<buttonurl:(?:/{0,2})(.+?)(:same)?\>
         "usage": [
             "{tr}ibutton <text> [Name on button]<buttonurl:link you want to open>",
         ],
-        "examples": "{tr}لستة قنواتي الرسمية [𝗧ُِٔ𝗢ٍَِ𝗢َِّ𝗟َٖ𝗦َ]<buttonurl:t.me/JepThon> [𝗝ََِ𝗠ٓ𝗧َُِْٓ𝗛ُ𝗢َ𝗡ٍَ]<buttonurl:t.me/JepThon> ",
+        "examples": "{tr}لستة قنواتي الرسمية [𝗧ُِٔ𝗢ٍَِ𝗢َِّ𝗟َٖ𝗦َ]<buttonurl:t.me/Repthon> [R𝙀𝙋𝙏𝙃𝙊𝙉𖠏]<buttonurl:t.me/Repthon> ",
     },
 )
 async def _(event):
