64de423cafe27155cb7987208dc55d7d8b507f72
روجر <104784913+rogerpq@users.noreply.github.com>
2022-09-27 18:38:47 +0300

    Update اوامر التشغيل.py

--- jepthon/plugins/اوامر التشغيل.py
+++ jepthon/plugins/اوامر التشغيل.py
@@ -30,10 +30,10 @@ plugin_category = "tools"
 async def _(event):
     "Restarts the bot !!"
     if BOTLOG:
-        await event.client.send_message(BOTLOG_CHATID, "**⌔︙جيبثون العرب ↻** \n" "**⌯︙تم تحديث السورس بنجاح ✅ ↻**")
+        await event.client.send_message(BOTLOG_CHATID, "**⌔︙ ريبثون ↻** \n" "**⌯︙تم تحديث السورس بنجاح ✅ ↻**")
     lMl10l = await edit_or_reply(
         event,
-        "**⌯︙جـاري تحديث سورس جيبثون ، قـد يستغـرق الأمـر 2-3 دقائـق لاتقم بكتابة تحديث مـره اخـرى انتـظـر ⏱**",
+        "**⌯︙جـاري تحديث سورس ريبثون ، قـد يستغـرق الأمـر 2-3 دقائـق لاتقم بكتابة تحديث مـره اخـرى انتـظـر ⏱**",
     )
     try:
         ulist = get_collectionlist_items()
