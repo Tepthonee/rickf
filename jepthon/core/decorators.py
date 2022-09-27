505ce028752718bbedfbbe6c8add6a502ce80a44
روجر <104784913+rogerpq@users.noreply.github.com>
2022-09-27 19:54:07 +0300

    Update decorators.py

--- jepthon/core/decorators.py
+++ jepthon/core/decorators.py
@@ -22,7 +22,7 @@ def check_owner(func):
         else:
             HELP_TEXT = (
                 gvarstatus("HELP_TEXT")
-                or "هذا الخيار ليس لك يجب عليك تنصيب سورس جيبثون بنفسك  @Jepthon"
+                or "هذا الخيار ليس لك يجب عليك تنصيب سورس ريبثون بنفسك  @Repthon"
             )
             await c_q.answer(
                 HELP_TEXT,
