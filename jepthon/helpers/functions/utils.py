9ed5f205e73b272209765158992b974346183f20
روجر <104784913+rogerpq@users.noreply.github.com>
2022-09-28 00:41:08 +0300

    Update utils.py

--- jepthon/helpers/functions/utils.py
+++ jepthon/helpers/functions/utils.py
@@ -1,7 +1,6 @@
 import re
 import time
 from datetime import datetime
-
 from telethon.tl.types import Channel, PollAnswer
 
 
@@ -103,4 +102,4 @@ def deEmojify(inputString: str) -> str:
 
 def soft_deEmojify(inputString: str) -> str:
     """Remove emojis and other non-safe characters from string"""
-    return get_emoji_regexp().sub("", inputString)
+    return re.sub("[^a-zA-Z0-9 \\`~!@#$%^&*(){}[\]_+=.:;\n'\",><?/-]", "", inputString)
