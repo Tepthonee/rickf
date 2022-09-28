f6232f81fc3b5d0a40a97da78bda4b72c281b597
روجر <104784913+rogerpq@users.noreply.github.com>
2022-09-28 11:56:33 +0300

    Update فحص.py

--- jepthon/plugins/فحص.py
+++ jepthon/plugins/فحص.py
@@ -74,7 +74,7 @@ async def amireallyalive(event):
 temp = """{ALIVE_TEXT}
 **‎{EMOJI}‌‎𝙽𝙰𝙼𝙴 𖠄 {mention}** ٫
 **‌‎{EMOJI}‌‎𝙿𝚈𝚃𝙷𝙾𝙽 𖠄 {pyver}** ٫
-**‌‎{EMOJI}‌‎R𝙀𝙋𝙏𝙃𝙊𝙉𖠏 𖠄 {Repver}** ٫
+**‌‎{EMOJI}‌‎R𝙀𝙋𝙏𝙃𝙊𝙉𖠏 𖠄 {jmver}** ٫
 **‌‎{EMOJI}‌‎𝚄𝙿𝚃𝙸𝙼𝙴 𖠄 {uptime}** ٫
 ‌‎**{EMOJI}‌‎‌‎𝙿𝙸𝙽𝙶 𖠄 {ping}** ٫
 **𖠄 𝐑𝐄𝐏𝐓𝐇𝐎𝐍 𝗨𝘀𝗲𝗿𝗯𝗼𝘁 𖠄**"""
