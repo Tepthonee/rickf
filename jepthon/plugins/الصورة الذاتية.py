a380c9bd57fe9d1c01805e16e3cacce8d5a5965c
روجر <104784913+rogerpq@users.noreply.github.com>
2022-10-07 12:37:51 +0300

    Update الصورة الذاتية.py

--- jepthon/plugins/الصورة الذاتية.py
+++ jepthon/plugins/الصورة الذاتية.py
@@ -1,21 +1,19 @@
 from jepthon import *
 from jepthon import jepiq
-from ..Config import Config
 from ..sql_helper.globals import gvarstatus
 
-Jepthon_CMD = Config.SCPIC_CMD or "ذاتية"
-@jepiq.on(admin_cmd(pattern=f"{Jepthon_CMD}"))
+@jepiq.on(admin_cmd(pattern="(ذاتية|ذاتيه)"))
 async def dato(event):
     if not event.is_reply:
         return await event.edit("..")
     lMl10l = await event.get_reply_message()
     pic = await lMl10l.download_media()
-    SC_TEXT = gvarstatus("SC_TEXT") or "**احا 😍**"
     await bot.send_file(
         "me",
         pic,
         caption=f"""
--تـم جـلب الصـورة بنجـاح ✅
+- تـم جـلب الصـورة بنجـاح ✓ 
+- غير مبري الذمه اذا استخدمت الامر للابتزاز
 - CH: @Repthon
 - Dev: @ZQ_LO
   """,
