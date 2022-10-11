5ff350a700ded00394abb75317573ca1b23fc744
روجر <104784913+rogerpq@users.noreply.github.com>
2022-10-11 21:36:30 +0300

    Update __init__.py

--- jepthon/__init__.py
+++ jepthon/__init__.py
@@ -4,7 +4,7 @@ import heroku3
 
 from .Config import Config
 from .core.logger import logging
-from .core.session import repiq
+from .core.session import jepiq
 from .sql_helper.globals import addgvar, delgvar, gvarstatus
 
 __version__ = "1.0.0"
