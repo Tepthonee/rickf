0a9c076d570e43b7e9829a392e264b5686a4d6cc
روجر <104784913+rogerpq@users.noreply.github.com>
2022-10-17 19:54:42 +0300

    Update ميمز.py

--- jepthon/plugins/ميمز.py
+++ jepthon/plugins/ميمز.py
@@ -204,7 +204,7 @@ async def jepmeme(memejep):
   url = f"https://t.me/MemeSoundJep/19"
   await memejep.client.send_file(memejep.chat_id,url,caption="",parse_mode="html",reply_to=Jep)
   await memejep.delete()
-@jepiq.on(admin_cmd(outgoing=True, pattern="3زيج$"))
+@jepiq.on(admin_cmd(outgoing=True, pattern="زيج3$"))
 async def jepmeme(memejep):
   Jep = await reply_id(memejep)
   url = f"https://t.me/illlil0l/7"
