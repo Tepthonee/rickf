c2ecab9114e98298216e363cf417c029e85f37e1
روجر <104784913+rogerpq@users.noreply.github.com>
2022-10-30 20:17:08 +0300

    Update ميمز.py

--- jepthon/plugins/ميمز.py
+++ jepthon/plugins/ميمز.py
@@ -87,7 +87,7 @@ async def _(event):
 async def jepvois(vois):
   rl = random.randint(3,267)
   url = f"https://t.me/DwDi1/{rl}"
-  await vois.client.send_file(vois.chat_id,url,caption="᯽︙ BY : @JEPTHON 🎀",parse_mode="html")
+  await vois.client.send_file(vois.chat_id,url,caption="᯽︙ BY : @Repthon 🎀",parse_mode="html")
   await vois.delete()
 
 @jepiq.on(admin_cmd(outgoing=True, pattern="شعر$"))
