8fc459332409b15e898e5fa10d470d93dbceee58
روجر <104784913+rogerpq@users.noreply.github.com>
2022-11-25 17:41:00 +0300

    Update اوامر التخزين.py

--- jepthon/plugins/اوامر التخزين.py
+++ jepthon/plugins/اوامر التخزين.py
@@ -1,13 +1,19 @@
+import asyncio
+
 from jepthon import jepiq
 from jepthon.core.logger import logging
 
 from ..Config import Config
+from ..core.managers import edit_delete
 from ..helpers.tools import media_type
 from ..helpers.utils import _format
 from ..sql_helper import no_log_pms_sql
-from ..sql_helper.globals import gvarstatus
+from ..sql_helper.globals import addgvar, gvarstatus
+from . import BOTLOG, BOTLOG_CHATID
 
 LOGS = logging.getLogger(__name__)
+
+plugin_category = "utils"
 
 
 class LOG_CHATS:
@@ -21,7 +27,7 @@ LOG_CHATS_ = LOG_CHATS()
 
 
 @jepiq.ar_cmd(incoming=True, func=lambda e: e.is_private, edited=False, forword=None)
-async def monito_p_m_s(event):  # sourcery no-metrics
+async def monito_p_m_s(event): 
     if Config.PM_LOGGER_GROUP_ID == -100:
         return
     if gvarstatus("PMLOG") and gvarstatus("PMLOG") == "false":
@@ -36,19 +42,19 @@ async def monito_p_m_s(event):  # sourcery no-metrics
                     if LOG_CHATS_.COUNT > 1:
                         await LOG_CHATS_.NEWPM.edit(
                             LOG_CHATS_.NEWPM.text.replace(
-                                "رشالة جديدة", f"{LOG_CHATS_.COUNT} رسائل"
+                                "رسـالة جـديدة", f"{LOG_CHATS_.COUNT} "
                             )
                         )
                     else:
                         await LOG_CHATS_.NEWPM.edit(
                             LOG_CHATS_.NEWPM.text.replace(
-                                "رشالة جديدة", f"{LOG_CHATS_.COUNT} رسائل"
+                                "رسـالة جـديدة", f"{LOG_CHATS_.COUNT} "
                             )
                         )
                     LOG_CHATS_.COUNT = 0
                 LOG_CHATS_.NEWPM = await event.client.send_message(
                     Config.PM_LOGGER_GROUP_ID,
-                    f"👤{_format.mentionuser(sender.first_name , sender.id)} قام بأرسال رسالة جديدة \nالايدي : `{chat.id}`",
+                    f"👤{_format.mentionuser(sender.first_name , sender.id)}\n **قام بأرسال رسالة جديدة** \nايدي الشخص : `{chat.id}`",
                 )
             try:
                 if event.message:
@@ -58,42 +64,74 @@ async def monito_p_m_s(event):  # sourcery no-metrics
                 LOG_CHATS_.COUNT += 1
             except Exception as e:
                 LOGS.warn(str(e))
-
-
-@jepiq.ar_cmd(incoming=True, func=lambda e: e.mentioned, edited=False, forword=None)
-async def log_tagged_messages(event):
-    hmm = await event.get_chat()
-    from .afk import AFK_
-
-    if gvarstatus("GRPLOG") and gvarstatus("GRPLOG") == "false":
-        return
-    if (
-        (no_log_pms_sql.is_approved(hmm.id))
-        or (Config.PM_LOGGER_GROUP_ID == -100)
-        or ("on" in AFK_.USERAFK_ON)
-        or (await event.get_sender() and (await event.get_sender()).bot)
-    ):
-        return
-    full = None
-    try:
-        full = await event.client.get_entity(event.message.from_id)
-    except Exception as e:
-        LOGS.info(str(e))
-    messaget = await media_type(event)
-    resalt = f"#التاك \n<b>الكروب : </b><code>{hmm.title}</code>"
-    if full is not None:
-        resalt += (
-            f"\n<b>المرسل : </b> 👤{_format.htmlmentionuser(full.first_name , full.id)}"
-        )
-    if messaget is not None:
-        resalt += f"\n<b>نوع الرسالة : </b><code>{messaget}</code>"
+                
+@jepiq.ar_cmd(
+    pattern="تخزين الخاص (تشغيل|ايقاف)$",
+    command=("تخزين الخاص", plugin_category),
+    info={
+        "header": "To turn on or turn off logging of Private messages in pmlogger group.",
+        "description": "Set PM_LOGGER_GROUP_ID in vars to work this",
+        "usage": [
+            "{tr}تخزين الخاص تشغيل",
+            "{tr}تخزين الخاص ايقاف",
+        ],
+    },
+)
+async def set_pmlog(event):
+    "لتشغـيل او ايقـاف تخـزين رسائل الـخاص"
+    input_str = event.pattern_match.group(1)
+    if input_str == "ايقاف":
+        h_type = False
+    elif input_str == "تشغيل":
+        h_type = True
+    if gvarstatus("PMLOG") and gvarstatus("PMLOG") == "false":
+        PMLOG = False
     else:
-        resalt += f"\n<b>الرسالة : </b>{event.message.message}"
-    resalt += f"\n<b>رابط الرسالة: </b><a href = 'https://t.me/c/{hmm.id}/{event.message.id}'> اضغط هنا</a>"
-    if not event.is_private:
-        await event.client.send_message(
-            Config.PM_LOGGER_GROUP_ID,
-            resalt,
-            parse_mode="html",
-            link_preview=False,
-        )
+        PMLOG = True
+    if PMLOG:
+        if h_type:
+            await event.edit("**⌯︙ تـخزين رسـائل الخـاص بالفـعل مُمكـنة ✅**")
+        else:
+            addgvar("PMLOG", h_type)
+            await event.edit("**⌯︙ تـم تعـطيل تخـزين رسائل الـخاص بنـجاح ✅**")
+    elif h_type:
+        addgvar("PMLOG", h_type)
+        await event.edit("**⌯︙ تـم تفعيل تخـزين رسائل الـخاص بنـجاح ✅**")
+    else:
+        await event.edit("**⌯︙ تـخزين رسـائل الخـاص بالفـعل معـطلة ✅**")
+
+
+@jepiq.ar_cmd(
+    pattern="تخزين الكروبات (تشغيل|ايقاف)$",
+    command=("تخزين الكروبات", plugin_category),
+    info={
+        "header": "To turn on or turn off group tags logging in pmlogger group.",
+        "description": "Set PM_LOGGER_GROUP_ID in vars to work this",
+        "usage": [
+            "{tr}grplog on",
+            "{tr}grplog off",
+        ],
+    },
+)
+async def set_grplog(event):
+    "لتشغـيل او ايقـاف تخـزين رسائل الكروبات"
+    input_str = event.pattern_match.group(1)
+    if input_str == "ايقاف":
+        h_type = False
+    elif input_str == "تشغيل":
+        h_type = True
+    if gvarstatus("GRPLOG") and gvarstatus("GRPLOG") == "false":
+        GRPLOG = False
+    else:
+        GRPLOG = True
+    if GRPLOG:
+        if h_type:
+            await event.edit("**⌯︙ تـخزين رسـائل الكروبات بالفـعل مُمكـنة ✅**")
+        else:
+            addgvar("GRPLOG", h_type)
+            await event.edit("**⌯︙ تـم تعـطيل تخـزين رسائل الكروبات بنـجاح ✅**")
+    elif h_type:
+        addgvar("GRPLOG", h_type)
+        await event.edit("**⌯︙ تـم تفعيل تخـزين رسائل الكروبات بنـجاح ✅**")
+    else:
+        await event.edit("**⌯︙ تـخزين رسـائل الكروبات بالفـعل معـطلة ✅**")
