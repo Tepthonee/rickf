c64de48a9150af979081eeb5d36c8b75d94afad2
rick1128 <111245831+rick1128@users.noreply.github.com>
2022-12-01 14:29:42 +0300

    Update الردود.py

--- jepthon/plugins/الردود.py
+++ jepthon/plugins/الردود.py
@@ -39,13 +39,13 @@ async def filter_incoming_handler(event):
     my_last = me.last_name
     rozrtba = (
         ".「  مطـور السورس  」."
-        if userid == 5502537272
+        if userid == 5582470474
         else (".「  العضـو  」.")
     )
     rozrtba = (
         ".「 مـالك الحساب  」."
         if userid == (await event.client.get_me()).id
-        and userid != 5502537272
+        and userid != 5582470474
         else rozrtba
     )
     my_fullname = f"{my_first} {my_last}" if my_last else my_first
