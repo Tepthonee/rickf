d5640d505cbea80f85bc809140b1495159e77b29
rick1128 <111245831+rick1128@users.noreply.github.com>
2022-12-01 14:04:08 +0300

    Update منوع.py

--- jepthon/plugins/منوع.py
+++ jepthon/plugins/منوع.py
@@ -65,8 +65,8 @@ R = [
     "  ⓱ **⪼**  [جيت واي 🚨](https://t.me/gamee?game=Getaway)   \n"
     "  ⓲ **⪼**  [الالـوان 🔮](https://t.me/gamee?game=ColorHit)   \n"
     "  ⓳ **⪼**  [مدفع الكرات🏮](https://t.me/gamee?game=NeonBlaster)   \n"
-    "**-** مطور السورس **⪼ [𐇮 𓂐 𓆩✗ ¦ ↱𝐺𝑜𝑙 𝐷. 𝑅𝑜𝑔𝑒𝑟↲ ¦ ✗𓆪 𖠛 ](t.me/ZQ_LO)   \n"
-    "**-** قناة السورس **⪼ [𐇮 ريبثون العربي ](t.me/Repthon)   "
+    "**-** مطور السورس **⪼ [𐇮 𓂐 𓆩✗ ¦ ↱SAIF↲ ¦ ✗𓆪 𖠛 ](t.me/A9AA99A)   \n"
+    "**-** قناة السورس **⪼ [𐇮 ريك ثون العربي ](t.me/RICKTHON)   "
 ]
 
 @jepiq.on(admin_cmd(pattern="بلي$"))
