0da43af6abf44133bfb8208dd6f284428fbbc2d1
rick1128 <111245831+rick1128@users.noreply.github.com>
2022-12-01 15:35:25 +0300

    Update functions.py

--- jepthon/helpers/functions/functions.py
+++ jepthon/helpers/functions/functions.py
@@ -2,9 +2,10 @@ import json
 import os
 import zipfile
 from random import choice
+import random
 from textwrap import wrap
 from uuid import uuid4
-
+from urllib.parse import quote, unquote
 import requests
 from googletrans import Translator
 
@@ -225,6 +226,65 @@ async def getTranslate(text, **kwargs):
             await sleep(0.1)
     return result
 
+def translate(*args, **kwargs):
+    headers = {
+        "Referer": "https://translate.google.co.in",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/47.0.2526.106 Safari/537.36",
+        "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
+    }
+    x = requests.post(
+        "https://translate.google.co.in/_/TranslateWebserverUi/data/batchexecute",
+        headers=headers,
+        data=_package_rpc(*args, **kwargs),
+    ).text
+    response = ""
+    data = json.loads(json.loads(x[4:])[0][2])[1][0][0]
+    subind = data[-2]
+    if not subind:
+        subind = data[-1]
+    for i in subind:
+        response += i[0]
+    return response
+
+def _get_value(stri):
+    try:
+        value = eval(stri.strip())
+    except Exception as er:
+        value = stri.strip()
+    return value
+
+def _package_rpc(text, lang_src="auto", lang_tgt="auto"):
+    GOOGLE_TTS_RPC = ["MkEWBc"]
+    parameter = [[text.strip(), lang_src, lang_tgt, True], [1]]
+    escaped_parameter = json.dumps(parameter, separators=(",", ":"))
+    rpc = [[[random.choice(GOOGLE_TTS_RPC), escaped_parameter, None, "generic"]]]
+    espaced_rpc = json.dumps(rpc, separators=(",", ":"))
+    freq_initial = "f.req={}&".format(quote(espaced_rpc))
+    freq = freq_initial
+    return freq
+
+def safe_load(file, *args, **kwargs):
+    if isinstance(file, str):
+        read = file.split("\n")
+    else:
+        read = file.readlines()
+    out = {}
+    for line in read:
+        if ":" in line:  # Ignores Empty & Invalid lines
+            spli = line.split(":", maxsplit=1)
+            key = spli[0].strip()
+            value = _get_value(spli[1])
+            out.update({key: value or []})
+        elif "-" in line:
+            spli = line.split("-", maxsplit=1)
+            where = out[list(out.keys())[-1]]
+            if isinstance(where, list):
+                value = _get_value(spli[1])
+                if value:
+                    where.append(value)
+    return out
 
 def reddit_thumb_link(preview, thumb=None):
     for i in preview:
