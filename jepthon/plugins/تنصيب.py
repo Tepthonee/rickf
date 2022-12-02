1a5900104a3584affec835807feb0956511727bf
Tepthonee <115221367+Tepthonee@users.noreply.github.com>
2022-12-02 23:46:12 +0300

    Update تنصيب.py

--- jepthon/plugins/تنصيب.py
+++ jepthon/plugins/تنصيب.py
@@ -3,7 +3,7 @@ from ..Config import Config
 from ..sql_helper.globals import gvarstatus
 from JepIQ.razan.resources.mybot import *
 
-ROZ_PIC = "https://telegra.ph/file/632932f6b937df7d1ac4f.jpg"
+ROZ_PIC = "https://telegra.ph/file/f1e757035e56613a9ef92.jpg"
 
 if Config.TG_BOT_USERNAME is not None and tgbot is not None:
     @tgbot.on(events.InlineQuery)
@@ -13,7 +13,7 @@ if Config.TG_BOT_USERNAME is not None and tgbot is not None:
         query = event.text
         await bot.get_me()
         if query.startswith("السورس") and event.query.user_id == bot.uid:
-            buttons = [[Button.url("1- شرح التنصيب", "https://youtu.be/eXaxtjZPEj8"), Button.url("2- استخراج ايبيات", "https://my.telegram.org/"),],[Button.url("3- ستخراج تيرمكس", "https://replit.com/@saif61/generatestringsession#main.py"), Button.url("4- بوت فاذر", "http://t.me/BotFather"),],[Button.url("5- رابط التنصيب", "https://app.koyeb.com/apps/deploy?type=git&repository=github.com/rick1128/rickthoniq&branch=rickthon&name=rickthon&env[APP_ID]=ضع_ابب_ايدي&env[API_HASH]=ضع_ايبي_هاش&env[ENV]=ANYTHING&env[DATABASE_URL]=قاعدة_البيانات&env[STRING_SESSION]=كود_تريمكس&env[TG_BOT_USERNAME]=معرف_البوت&env[TG_BOT_TOKEN]=توكن_البوت&env[ALIVE_NAME]=اسم_المستخدم&env[TZ]=Asia/Baghdad"),],[Button.url("المطـور 👨🏼‍💻", "https://t.me/a9aa99a"),]]
+            buttons = [[Button.url("1- قناة السورس", "https://t.me/Tepthone"), Button.url("2- استخراج ايبيات", "https://my.telegram.org/"),],[Button.url("3- ستخراج تيرمكس", "https://replit.com/@saif61/generatestringsession#main.py"), Button.url("4- بوت فاذر", "http://t.me/BotFather"),],[Button.url("5- رابط التنصيب", "https://app.koyeb.com/apps/deploy?type=git&repository=github.com/Tepthonee/tepthoniq&branch=Tepthon&name=tepthon&env[APP_ID]=%D8%B6%D8%B9_%D8%A7%D8%A8%D8%A8_%D8%A7%D9%8A%D8%AF%D9%8A&env[API_HASH]=%D8%B6%D8%B9_%D8%A7%D9%8A%D8%A8%D9%8A_%D9%87%D8%A7%D8%B4&env[ENV]=ANYTHING&env[DATABASE_URL]=%D9%82%D8%A7%D8%B9%D8%AF%D8%A9_%D8%A7%D9%84%D8%A8%D9%8A%D8%A7%D9%86%D8%A7%D8%AA&env[STRING_SESSION]=%D9%83%D9%88%D8%AF_%D8%AA%D8%B1%D9%8A%D9%85%D9%83%D8%B3&env[TG_BOT_USERNAME]=%D9%85%D8%B9%D8%B1%D9%81_%D8%A7%D9%84%D8%A8%D9%88%D8%AA&env[TG_BOT_TOKEN]=%D8%AA%D9%88%D9%83%D9%86_%D8%A7%D9%84%D8%A8%D9%88%D8%AA&env[ALIVE_NAME]=%D8%A7%D8%B3%D9%85_%D8%A7%D9%84%D9%85%D8%B3%D8%AA%D8%AE%D8%AF%D9%85&env[TZ]=Asia/Baghdad&env[COMMAND_HAND_LER]=.&env[ELEPHANT_API_KEY]=%D8%B6%D8%B9_%D8%A7%D9%8A%D8%A8%D9%8A_%D9%83%D9%8A&env[KOYEB_APP_NAME]=%D8%B6%D8%B9_%D8%A7%D8%B3%D9%85_%D8%A7%D9%84%D8%AA%D8%B7%D8%A8%D9%8A%D9%82"),],[Button.url("المطـور 👨🏼‍💻", "https://t.me/PPF22"),]]
             if ROZ_PIC and ROZ_PIC.endswith((".jpg", ".png", "gif", "mp4")):
                 result = builder.photo(ROZ_PIC, text=ROZ, buttons=buttons, link_preview=False)
             elif ROZ_PIC:
