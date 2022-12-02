974b9fc6cfbdb2b1d6aec4361c608c9bffef52bc
Tepthonee <115221367+Tepthonee@users.noreply.github.com>
2022-12-02 23:48:40 +0300

    Update decorators.py

--- jepthon/utils/decorators.py
+++ jepthon/utils/decorators.py
@@ -157,8 +157,8 @@ def errors_handler(func):
             result = output[0] + output[1]
             ftext += result
             pastelink = await paste_message(ftext)
-            text = "**تقرير خطا ريك ثون**\n\n"
-            link = "[هنا](https://t.me/rickthon_group)"
+            text = "**تقرير خطا تيبثون**\n\n"
+            link = "[هنا](https://t.me/+NOkvLLXUKAU0ODRk)"
             text += "إذا كنت تريد يمكنك الإبلاغ عن ذلك"
             text += f"- فقط قم بإعادة توجيه هذه الرسالة {link}.\n"
             text +="لا يتم تسجيل اي خطا فقط التاريخ والوقت\n\n"
