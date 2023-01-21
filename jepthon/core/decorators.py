3dc41267bf497d543bb94d99e111cc250e544079
Arabic Source <115221367+Tepthonee@users.noreply.github.com>
2023-01-21 19:10:32 +0300

    Update decorators.py

--- jepthon/core/decorators.py
+++ jepthon/core/decorators.py
@@ -22,7 +22,7 @@ def check_owner(func):
         else:
             HELP_TEXT = (
                 gvarstatus("HELP_TEXT")
-                or "هذا الخيار ليس لك يجب عليك تنصيب سورس تيبثون بنفسك  @Tepthone"
+                or "هذا الخيار ليس لك يجب عليك تنصيب سورس تيبثون بنفسك  @Tepthon"
             )
             await c_q.answer(
                 HELP_TEXT,
