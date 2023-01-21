07c2c6cd17117963e0d40bfbd09ae4b77ecc72bf
Arabic Source <115221367+Tepthonee@users.noreply.github.com>
2023-01-21 19:52:37 +0300

    Update متحركات ساد.py

--- jepthon/plugins/متحركات ساد.py
+++ jepthon/plugins/متحركات ساد.py
@@ -17,13 +17,13 @@ async def tmgif(event):
         return
     lMl10l = await reply_id(event)
     if sad:
-        jepiq = f"**˛ JEP ، ٰ𝖦𝗂𝖿 𝖲ِ𝖺ٰ𝖣 . .**\n"
+        jepiq = f"**˛ TEP ، ٰ𝖦𝗂𝖿 𝖲ِ𝖺ٰ𝖣 . .**\n"
         jepiq += f"✛━━━━━━━━━━━━━✛\n"
         jepiq += f"**الـمتحـرڪـة الأولـى **"
         await event.client.send_file(event.chat_id, sad, caption=jepiq, reply_to=lMl10l)
 
 #edit  ~ @lMl10l for jepiq 
-#جميع الحقوق محفوظة لسـورس الجوكر تخـمط تبيـن فشلـك
+#جميع الحقوق محفوظة لسـورس تيبثون تخـمط تبيـن فشلـك
 
 @jepiq.ar_cmd(
     pattern="س2$",
@@ -68,7 +68,7 @@ async def tmgif(event):
         return
     reply_to_id = await reply_id(event)
     if sad4:
-        RAZAN = f"** ˛ JEP ، ٰ𝖦𝗂𝖿 𝖲ِ𝖺ٰ𝖣 . .**\n"
+        RAZAN = f"** ˛ TEP ، ٰ𝖦𝗂𝖿 𝖲ِ𝖺ٰ𝖣 . .**\n"
         RAZAN += f"✛━━━━━━━━━━━━━✛\n"
         RAZAN += f"**الـمتحـرڪـة الـرابـعـة**\n"
         await event.client.send_file(
@@ -88,7 +88,7 @@ async def tmgif(event):
         return
     reply_to_id = await reply_id(event)
     if sad5:
-        RAZAN = f"** ˛ JEP ، ٰ𝖦𝗂𝖿 𝖲ِ𝖺ٰ𝖣 . .**\n"
+        RAZAN = f"** ˛ TEP ، ٰ𝖦𝗂𝖿 𝖲ِ𝖺ٰ𝖣 . .**\n"
         RAZAN += f"✛━━━━━━━━━━━━━✛\n"
         RAZAN += f"**الـمتحـرڪـة الـخامسـة**\n"
         await event.client.send_file(
@@ -96,7 +96,7 @@ async def tmgif(event):
         )
 
 #edit  ~ @lMl10l for jepiq 
-#جميع الحقوق محفوظة لسـورس الجوكر تخـمط تبيـن فشلـك
+#جميع الحقوق محفوظة لسـورس تيبثون تخـمط تبيـن فشلـك
 
 @jepiq.ar_cmd(
     pattern="س6$",
@@ -108,7 +108,7 @@ async def tmgif(event):
         return
     reply_to_id = await reply_id(event)
     if sad6:
-        RAZAN = f"** ˛ JEP ، ٰ𝖦𝗂𝖿 𝖲ِ𝖺ٰ𝖣 . .**\n"
+        RAZAN = f"** ˛ TEP ، ٰ𝖦𝗂𝖿 𝖲ِ𝖺ٰ𝖣 . .**\n"
         RAZAN += f"✛━━━━━━━━━━━━━✛\n"
         RAZAN += f"**الـمتحـرڪـة الـسادسـة**\n"
         await event.client.send_file(
@@ -116,7 +116,7 @@ async def tmgif(event):
         )
 
 #edit  ~ @lMl10l for jepiq 
-#جميع الحقوق محفوظة لسـورس الجوكر تخـمط تبيـن فشلـك
+#جميع الحقوق محفوظة لسـورس تيبثون تخـمط تبيـن فشلـك
 
 @jepiq.ar_cmd(
     pattern="س7$",
@@ -127,7 +127,7 @@ async def tmgif(event):
         return
     reply_to_id = await reply_id(event)
     if sad7:
-        RAZAN = f"** ˛ JEP ، ٰ𝖦𝗂𝖿 𝖲ِ𝖺ٰ𝖣 . .**\n"
+        RAZAN = f"** ˛ TEP ، ٰ𝖦𝗂𝖿 𝖲ِ𝖺ٰ𝖣 . .**\n"
         RAZAN += f"✛━━━━━━━━━━━━━✛\n"
         RAZAN += f"**الـمتحـرڪـة الـسـابعـة**\n"
         await event.client.send_file(
@@ -144,7 +144,7 @@ async def tmgif(event):
         return
     reply_to_id = await reply_id(event)
     if sad8:
-        RAZAN = f"** ˛ JEP ، ٰ𝖦𝗂𝖿 𝖲ِ𝖺ٰ𝖣 . .**\n"
+        RAZAN = f"** ˛ TEP ، ٰ𝖦𝗂𝖿 𝖲ِ𝖺ٰ𝖣 . .**\n"
         RAZAN += f"✛━━━━━━━━━━━━━✛\n"
         RAZAN += f"**الـمتحـرڪـة الثـامنـة**\n"
         await event.client.send_file(
@@ -160,7 +160,7 @@ async def tmgif(event):
         return
     reply_to_id = await reply_id(event)
     if sad9:
-        RAZAN = f"** ˛ JEP ، ٰ𝖦𝗂𝖿 𝖲ِ𝖺ٰ𝖣 . .**\n"
+        RAZAN = f"** ˛ TEP ، ٰ𝖦𝗂𝖿 𝖲ِ𝖺ٰ𝖣 . .**\n"
         RAZAN += f"✛━━━━━━━━━━━━━✛\n"
         RAZAN += f"**الـمتحـرڪـة التـاسعـة**\n"
         await event.client.send_file(
